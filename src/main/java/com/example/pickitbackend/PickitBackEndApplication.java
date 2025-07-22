28c9cca9a00dcba33ae305a9434def83a364220e
qpry42 <ky020408@naver.com>
2025-07-22 16:33:10 +0900

    Initial commit

---
+++ src/main/java/com/example/pickitbackend/PickitBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.pickitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickitBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickitBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pickitbackend/PickitBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pickitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickitBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
