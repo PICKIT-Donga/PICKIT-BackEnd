4ce4a6b565a472ed8338b9696c451f04a1391ac2
[Github <ggamji09@naver.com>
2025-07-30 06:05:19 +0900

    fix : Dto 변경에 따른 Controller 수정

--- src/main/java/com/example/pickitbackend/controller/PopupController.java
+++ src/main/java/com/example/pickitbackend/controller/PopupController.java
@@ -1,8 +1,7 @@
 package com.example.pickitbackend.controller;
 
 
-import com.example.pickitbackend.dto.PopupRequestDto;
-import com.example.pickitbackend.dto.PopupResponseDto;
+import com.example.pickitbackend.dto.*;
 import com.example.pickitbackend.service.PopupService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.Operation;
@@ -22,32 +21,30 @@ public class PopupController {
     // 팝업 저장 API
     @PostMapping("/save")
     @Operation(summary = "팝업 저장", description = "팝업을 저장합니다.")
-    public ResponseEntity<PopupResponseDto> createPopup(PopupRequestDto popupRequestDto) {
-        PopupResponseDto response = popupService.savePopup(popupRequestDto);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<PopupDetailResponseDto> createPopup(@RequestBody PopupRequestDto popupRequestDto) {
+        return ResponseEntity.ok(popupService.savePopup(popupRequestDto));
     }
 
     // 팝업 전체 조회 API
     @GetMapping("/all")
     @Operation(summary = "모든 팝업 조회", description = "저장된 모든 팝업을 조회합니다.")
-    public ResponseEntity<List<PopupResponseDto>> getAllPopups() {
-        List<PopupResponseDto> response = popupService.getAllPopups();
+    public ResponseEntity<List<PopupListResponseDto>> getAllPopups() {
+        List<PopupListResponseDto> response = popupService.getAllPopups();
         return ResponseEntity.ok(response);
     }
 
     // 지역별 팝업 조회 API
     @GetMapping("/address")
     @Operation(summary = "지역별 팝업 조회", description = "특정 지역의 팝업을 조회합니다.")
-    public ResponseEntity<List<PopupResponseDto>> getPopupsByAddress(String address) {
-        List<PopupResponseDto> response = popupService.getPopupsByAddress(address);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<List<PopupAddressResponseDto>> getPopupsByAddress(@RequestParam String address) {
+        return ResponseEntity.ok(popupService.getPopupsByAddress(address));
     }
 
     // 팝업 ID로 단일 팝업 조회 API
     @GetMapping("/{id}")
     @Operation(summary = "팝업 ID로 조회", description = "ID로 특정 팝업을 조회합니다.")
-    public ResponseEntity<PopupResponseDto> getPopupById(@PathVariable Long id) {
-        PopupResponseDto response = popupService.getPopupById(id);
+    public ResponseEntity<PopupDetailResponseDto> getPopupById(@PathVariable Long id) {
+        PopupDetailResponseDto response = popupService.getPopupById(id);
         return ResponseEntity.ok(response);
     }
 
